47e7c8f455c18d4742403ab8c5fe3e27e04cd39a
momen186 <momenehab186@gmail.com>
2025-05-10 18:18:14 +0300

    added unlock user feature

---
+++ unlock_user.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+user=$1
+
+# Check if username was provided
+if [ -z "$user" ]; then
+  echo "Usage: $0 username"
+  exit 1
+fi
+
+# Check if user exists
+if id "$user" &>/dev/null; then
+sudo passwd -u "$user"
+else
+  echo "❌ User '$user' does not exist."
+  exit 1
+fi
